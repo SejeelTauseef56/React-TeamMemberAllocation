8c01184d3e4d6c22cc264f77f086a256758ab3d8
SejeelTauseef1 <6804906-SejeelTauseef1@users.noreply.replit.com>
2023-04-19 17:46:02 +0000

    Initial commit

---
+++ src/NotFound.jsx
@@ -0,0 +1,16 @@
+const NotFound = () => {
+
+  return (
+    <header className = 'container'>
+      <div className = 'row justify-content-center mt-3 mb-4'>
+      <div className = 'col-8'> 
+    <h1 className ="text-danger">404 - Page Not Found</h1>
+        <p className ="text-danger">The address may be incorrect, or the page may have moved.</p>
+
+
+</div>
+        </div>
+    </header>
+  )
+}
+export default NotFound
